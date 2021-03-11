1960c31619c930306a3119bf27ad8497f6ed8a71
Piotr <78019091+Po3mTo@users.noreply.github.com>
2021-03-11 16:01:44 +0100

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+print("Hello Again.")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+#I like coding expecially when I know it'll be the greatest adventure ever!
+#Thank you uncle for that opportunity! You're the best!
+print(f'My name is Piotr and I want to say Hello to my new beggining!')
---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("Now I will count my cars.")
+# It count and give solution.
+print(3 + 2 - 2 + 1 / 3)
+
+print("How about eggs? Meybe we should count them?")
+# It will give us the result of entire operation
+print(float( 3.0 / 1.0 * 5.0 + 8.0 / 2.0 - 5.0))
+
+print("Is it true that 3+2 < 5-7?")
+# if operation is false it will print "False", and if true - "True"
+print(3.0 + 2.0 < 5.0 - 7.0)
+# answers for our questions
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+
+print("Oh, that's why it's False.")
+
+print("How about some more?")
+# same things that happend above
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
