c1441e9b343304f4fbfb8ec7eb2cbdb0cc221a87
Piotr <78019091+Po3mTo@users.noreply.github.com>
2021-03-12 12:36:11 +0100

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,19 @@
+car_brands = "Lamborghini, Ferrari, Porsche, Mclaren, Audi"
+# znak \n oznacza, że wyliczamy jakby i każdy wyraz czy też zdanie jest od nowej linijki.
+car_models = "Huracan\nPista\n911\n650lt\nR8"
+
+print('Those are car brands: ', car_brands)
+print('Those are car models: ', car_models)
+
+
+# Dzięki tym 3 cudzysłowiom (""") z początku i końca można wprowadzać nieograniczoną ilość danych. No nieźle!
+print("""
+Mogę napisać tyle linijek ile mi sie podoba
+Równie dobrze mogę wkleić tekst jakiejś piosenki, albo artykułu.
+N
+I
+C
+Się nie dzieje jak mam więcej linijek. A to
+za sprawą cudzysłowi
+""")
+#bardzo przydatna opcja. Pewnie będę jej używał w przyszłości na codzień
