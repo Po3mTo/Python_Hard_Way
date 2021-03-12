f0ef7236195619a9e00b71861753ed437024c5da
Piotr <78019091+Po3mTo@users.noreply.github.com>
2021-03-12 12:17:11 +0100

    Add files via upload

--- ex8.py
+++ ex8.py
@@ -12,3 +12,10 @@ print(formatter.format(
     "Pretty good as a boy who do not know math."
 ))
 # Good tool to work faster. It wasn't in the scrimba course.
+# Jak to działa? Co przez to daję znać komputerowi?
+# 1. Weź łańcuch formatyzatora zdefiniowany w linii 1.
+# 2. Wywołaj jego funkcję formatowania, co jest podobne do powiedzenia mu, aby wykonał polecenie wiersza poleceń o nazwie format.
+# 3. Przekaż do formatu cztery argumenty, które odpowiadają czterem {} w zmiennej formatter.
+# Jest to podobne do przekazania argumentów do polecenia wiersza poleceń format.
+# 4. Wynikiem wywołania formatu na formatterze jest nowy łańcuch, który ma {} zastąpione czterema zmiennymi.
+# To jest właśnie to, co drukuje teraz 'print'.
