b34eec0762057ccb402ad9867b8e1d61d344154c
Piotr <78019091+Po3mTo@users.noreply.github.com>
2021-03-12 10:36:17 +0100

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,10 @@
+my_name = 'Piotr Mika'
+my_age = 20 # but in May 21
+my_height = 71.25 # inches , but in cm: 181
+my_eyes = 'brown'
+my_teeth = 'white' # I think...haha
+my_hair = 'dark blond'
+
+print(f'Hi my name is {my_name} and im {my_age} years old.')
+print(f'Also im {my_height} inches tall and have {my_eyes} eyes. I think my teeth are {my_teeth} and hair {my_hair}.')
+print()
