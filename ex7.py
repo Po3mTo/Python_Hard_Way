d0f7b0dd35514f2f411616f48ac037426ff801b8
Piotr <78019091+Po3mTo@users.noreply.github.com>
2021-03-12 11:54:11 +0100

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,23 @@
+print("Joseph has a big brown cat.")
+print("The cats name was Dingo and his fur was brown as {}".format('bear'))
+print("." * 54)
+
+end1 = "I"
+end2 = "l"
+end3 = "i"
+end4 = "k"
+end5 = "e"
+end6 = "p"
+end7 = "r"
+end8 = "o"
+end9 = "g"
+end10 = "r"
+end11 = "a"
+end12 = "m"
+end13 = "m"
+end14 = "i"
+end15 = "n"
+end16 = "g"
+
+print(end1 + " " + end2 + end3 + end4 + end5 + " " + end6 + end7 + end8 + end9 + end10, end = '')
+print(end11 + end12 + end13 + end14 + end15 + end16)
