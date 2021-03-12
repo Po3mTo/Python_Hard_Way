5efa4184e79817916c144be6f8dfe6c2d3b2b0f3
Piotr <78019091+Po3mTo@users.noreply.github.com>
2021-03-12 11:27:28 +0100

    Add files via upload

---
+++ ex6.py
@@ -0,0 +1,13 @@
+types_of_cars = 11
+bad_idea = False
+x = 'I should buy another 5 cars to my collection. I already have '
+y = "but what do you think about bike?"
+
+
+sentence = (f'{x}{types_of_cars}, {y}')
+print(sentence)
+print(f'Is that a good idea? {bad_idea}')
+
+very_exciting = True
+evaluation = "Isn't that so exciting?! {}"
+print(evaluation.format(very_exciting))
