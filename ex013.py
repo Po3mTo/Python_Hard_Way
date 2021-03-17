22a3fb80664e69602da8151723fded5349089fa7
Piotr <78019091+Po3mTo@users.noreply.github.com>
2021-03-17 17:04:25 +0100

    Add files via upload

---
+++ ex013.py
@@ -0,0 +1,23 @@
+from sys import argv
+
+# argv = argument variable
+# Powinieneś mieć tyle samo argumentów tu i w terminalu.
+# by włączyć to w terminalu: python3.9 ex013.py(to jest script) rabbit house orange blueberry bat
+#script, rabbit, house, orange, blueberry, bat = argv
+
+#print("The script is called:", script)
+#print("The first variable is called:", rabbit)
+#print("My second variable is called:", house)
+#print("And my last variable is called:", orange)
+#print("Fourth variable is called:", blueberry)
+#print("Fifth varialbe is called:", bat)
+
+#script, two, three = argv
+
+#print("Name of the file:", script)
+#input('')
+#print("Third variable is called:", three)
+
+script, czekolada = argv
+print("Name of the file:", script)
+input('' and two)
