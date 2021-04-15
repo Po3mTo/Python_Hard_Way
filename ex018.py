f9d60a0a1f4237a253811698b6f1a4eae8db467a
Piotr <78019091+Po3mTo@users.noreply.github.com>
2021-04-15 13:39:36 +0200

    Add files via upload

--- ex018.py
+++ ex018.py
@@ -3,6 +3,9 @@ def print_two(*args):
     arg1, arg2 = args
     print(f"arg1: {arg1}, arg2: {arg2}")
 
+def print_two_again(arg1,arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
 # this just takes one argument
 def print_one(arg1):
      print(f"arg1: {arg1}")
@@ -13,7 +16,7 @@ def print_none():
 
 
 
-print_two("Piotr","Mika")
-print_two_again("Piotr","Mika")
+print_two("Piotr", "Mika")
+print_two_again("Piotr", "Mika")
 print_one("First!")
 print_none()
